cc60cd6ace5af9e87ea6cd192c8838dade0b7b5f
Macbook Air <macbookair@David-Ramirez.netgear.com>
2017-02-15 16:49:44 -0800

    add server side routes ready to add the frontend

--- routes/articles.js
+++ routes/articles.js
@@ -1,9 +1,87 @@
 var express = require('express');
 var router = express.Router();
 
+var Article = require('../models/article')
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  Article.getArticles(function(err, articles){
+    if(err){
+      console.log(err);
+    }
+    res.json(articles)
+  })
+});
+
+router.get('/:id', function(req, res, next) {
+  Article.getArticleById(req.params.id, function(err, articles){
+    if(err){
+      console.log(err);
+    }
+    res.json(articles)
+  })
+});
+
+
+router.get('/category/:category', function(req, res, next) {
+  Article.getArticlesByCategory(req.params.category, function(err, articles){
+    if(err){
+      console.log(err);
+    }
+    res.json(articles)
+  })
+});
+
+router.post('/', function(){
+  //Get the form values
+  var title = req.body.title;
+  var category = req.body.category;
+  var body = req.body.body;
+
+//Article Object
+  var newArticle = new Article({
+    title: title,
+    category: category,
+    body: body
+  });
+
+  Article.createArticle(newArticle, function(err, article){
+    if(err){
+      console.log(err);
+    }
+    res.location('/articles');
+    res.redirect('/articles');
+  });
+});
+
+//Update Article
+router.put('/', function(req, res, next){
+  var id = req.body.id;
+  var data = {
+    title: req.body.title,
+    category: req.body.category,
+    body: req.body.body
+  };
+
+  //Create Article
+  Article.updateArticle(id, data, function(err, article){
+    if(err){
+      console.log(err);
+    }
+    res.location('/articles');
+    res.redirect('/articles');
+  });
+});
+
+router.delete('/:id', function(req, res, next){
+  var id  = req.params.id;
+
+  Article.removeArticle(id, function(err, article){
+    if(err){
+      console.log(err);
+    }
+    res.location('/articles');
+    res.redirect('/articles');
+  });
 });
 
 module.exports = router;
--- routes/categories.js
+++ routes/categories.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+var Category = require('../models/category.js')
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  Article.getCategories(function(err, categories){
+    if(err){
+      console.log(err);
+    }
+    res.json(categories)
+  })
+});
+
+
+router.get('/:id', function(req, res, next) {
+  Article.getCategoryById(req.params.id, function(err, categories){
+    if(err){
+      console.log(err);
+    }
+    res.json(categories)
+  })
 });
 
 module.exports = router;
